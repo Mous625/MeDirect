78416de8f4a133c58d7519efd6fe375b46daf0aa
Andre Mousu <amousu8@gmail.com>
2025-10-27 21:23:37 +0100

    health check

---
+++ ExchangeSystem.Host.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeSystem.Host.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
